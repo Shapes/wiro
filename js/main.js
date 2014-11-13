eca7385284f485f263fa731c72a674c15fad6893
Marko Balažic <marko.balazic@gmail.com>
2014-11-13 21:34:00 +0100

    premikanje po liniji

--- js/main.js
+++ js/main.js
@@ -1,18 +1,38 @@
 function start(){
     var wrapper = document.getElementById('wrapper');
-    var scene, camera, renderer;
-    var geometry, material, mesh;
+    var scene, camera, renderer, splineCamera, cameraEye;
+    var geometry, material, mesh, tube, animation=true,animiraj=true;
     var width=1000, height=600;
 
+    var targetRotation = 0;
+    var targetRotationOnMouseDown = 0;
+
+    var mouseX = 0;
+    var mouseXOnMouseDown = 0;
+
+    var windowHalfX = window.innerWidth / 2;
+    var windowHalfY = window.innerHeight / 2;
+
+    var binormal = new THREE.Vector3();
+    var normal = new THREE.Vector3();
+
     init();
-    //animate();
+    animate();
 
     function init() {
 
         scene = new THREE.Scene();
         
-        var light = new THREE.DirectionalLight( 0xffffff );
-        light.position.set( 0, 0, 3 );
+        
+        light = new THREE.DirectionalLight( 0xffffff );
+        light.position.set( 1, 1, 1 );
+        scene.add( light );
+ 
+        light = new THREE.DirectionalLight( 0x002288 );
+        light.position.set( -1, -1, -1 );
+        scene.add( light );
+ 
+        light = new THREE.AmbientLight( 0x222222 );
         scene.add( light );
 
         camera = new THREE.PerspectiveCamera(50, width / height, 0.1, 1000 );
@@ -80,7 +100,7 @@ function start(){
     
         splineCamera = new THREE.PerspectiveCamera( 84, window.innerWidth / window.innerHeight, 0.01, 1000 );
         parent.add( splineCamera );
-        //splineCamera.lookAt(-195.21238109169673, 18.160346897694417, -81.49978092684051);
+        //splineCamera.rotateOnAxis(new THREE.Vector3(1, 0, 0), degInRad(10));
         
         tube = new THREE.TubeGeometry(new THREE.Curves.KnotCurve(), 200, 2, 3, true);
         addGeometry(tube, 0xF4A460);
@@ -91,6 +111,7 @@ function start(){
         wrapper.appendChild( renderer.domElement );
 
         renderer.render( scene, splineCamera );
+        renderer.setClearColor( 0xf0f0f0 );
     }
 
     function addGeometry( geometry, color ) {
@@ -107,6 +128,71 @@ function start(){
 
         parent.add( tubeMesh );
 
+    }
+
+    function animate() {
+        if(animiraj){
+			requestAnimationFrame( animate );
+			render();
+        }
+    }
+
+var lookAhead=true;
+
+    function render() {
+        
+        // Try Animate Camera Along Spline
+        var time = Date.now();                           // Trenutni ca
+        var looptime = 20 * 5000;                        // cas zanke ? HITROST 
+        var t = ( time % looptime ) / looptime;
+
+        var pos = tube.parameters.path.getPointAt( t );
+        //console.log(pos);
+        //pos.multiplyScalar(10);         
+        
+        // interpolation
+        var segments = tube.tangents.length;
+        var pickt = t * segments;
+        var pick = Math.floor( pickt );
+        var pickNext = ( pick + 1 ) % segments;
+
+        binormal.subVectors( tube.binormals[ pickNext ], tube.binormals[ pick ] );
+        binormal.multiplyScalar( pickt - pick ).add( tube.binormals[ pick ] );
+
+
+
+        var dir = tube.parameters.path.getTangentAt( t );
+
+        var offset = 2.8;
+
+        normal.copy( binormal ).cross( dir );
+
+        // We move on a offset on its binormal
+        pos.add( normal.clone().multiplyScalar( offset ) );
+        
+        splineCamera.position.copy( pos );
+        //cameraEye.position.copy( pos );
+
+
+        // Camera Orientation 1 - default look at
+        // splineCamera.lookAt( lookAt );
+
+        // Using arclength for stablization in look ahead.
+        var lookAt = tube.parameters.path.getPointAt( ( t + 30 / tube.parameters.path.getLength() ) % 1 ).multiplyScalar( 5 );
+
+        // Camera Orientation 2 - up orientation via normal
+        if (!lookAhead)
+        lookAt.copy( pos ).add( dir );
+        
+        splineCamera.matrix.lookAt(splineCamera.position, lookAt, normal);
+        splineCamera.rotation.setFromRotationMatrix( splineCamera.matrix,   splineCamera.rotation.order );
+
+
+
+        parent.rotation.y += ( targetRotation - parent.rotation.y ) * 0.05;
+
+        renderer.render( scene, splineCamera );
+        renderer.setClearColor( 0xf0f0f0 );
     }
 }
 
